fef2a155ca780e97dd234cc32c0d7d01ec283ff6
Florian <git@florian-a.dev>
2025-02-01 21:36:11 +0100

    add page3

---
+++ frontend/src/pages/Page1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page1() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">Hello</h1>
+    </div>
+  );
+}
+
+export default Page1;
