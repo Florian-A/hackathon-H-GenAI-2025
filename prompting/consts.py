d49d9c341692443998465f429b98563e94cd4a5a
Tristan Duquesne <20281069+Fulguritude@users.noreply.github.com>
2025-02-01 20:33:37 +0100

    feature(prompting): debugged prompting for agents 2 to 4

--- prompting/consts.py
+++ prompting/consts.py
@@ -1,4 +1,8 @@
-DEFAULT_CSV_FILE_PATH = "../../veolia-data-abonnements.csv"
+DEFAULT_CSVS = {
+	"../../veolia-data-abonnements.csv" : "This dataset contains contract information about water utility clients",
+	"../../veolia-data-factures.csv"    : "This dataset contains billing information about water utility clients",
+	"../../veolia-data-consos.csv"      : "This dataset contains consumption information about water utility clients",
+}
 DEFAULT_MISTRAL_MODEL = "mistral.mixtral-8x7b-instruct-v0:1"
 DEFAULT_MAX_TOKENS    = 4096
 DEFAULT_TEMPERATURE   = 0.5
@@ -11,9 +15,66 @@ system_prompt_metadata_describer = "When providing metadata information, you sho
 
 # system_prompt_metadata_improvement = "When suggesting corrections to metadata, you should clearly indicate what needs to be changed and why." 
 
+system_prompt_table_joiner = "When finding how to combine multiples tables, you should provide a list of features that you think act as keys to join the tables. "
+
 system_prompt_verif_feature_elicitation = "When identifying combinations of features that could elicit contradictions in rows, you should provide clear examples and explanations. "
 
 system_prompt_verif_value_elicitation = "When identifying specific values that seem like contradictory combinations, or single-feature impossible values, you should provide the relevant feature names, the specific values, and an explanation of why and how you think they might be contradictory. "
 
 system_prompt_sql_request = "When building an SQL request, make sure to use all the features and values in the context provided to build the appropriate SQL request. If intermediary features need to be computed, don't hesitate to do so. "
 
+"""
+The table factures with the following columns:
+CLE_ABONNEMENT
+Identifiant de l'abonné
+CLE_FACTURE
+Identifiant de la facture. Plusieurs factures peuvent être associées à un même abonné.
+DATE_EMISSION_FACTURE
+Date d'émission de la facture
+CONSO_FACTURE
+La consommation qui apparaît dans la facture, qui est facturé au consommateur
+DATE_RELEVE_INDEX_PRECEDENT_FACTURE_COMPOSITE
+La consommation d'une facture se calcule par la différence d'un index. L'index "d'aujourd'hui" moins l'index "d'hier". Ici il s'agit de la date de relevé de l'index le plus ancien ayant servi au calcul de la consommation.
+DATE_RELEVE_INDEX_FACTURE
+Date de relevé de l'index le plus récent ayant servi au calcul de la consommation.
+NB_FACTURES_PAR_PDS
+Le nombre de factures par point de service.
+NB_JOURS_CONNUS
+Le nombre de jours connus sur la facture (correspond normalement à la date de relevé index facture moins la date de relevé de l'index précédent)
+NUM_FAC_PAR_PDS
+L'ordre de la facture par point de service (1ère facture, 2ème facture, ...)
+
+The table consommation with the following columns:
+CLE_PDS
+L'identifiant du point de livraison. Le PDS se trouve dans l'identifiant abonnement. L'identifiant abonnement est composé de la manière suivante : "CLE_PDS" + ordre de l'abonné sur le point de service (par exemple "01", "02", ...). Un abonné peut effectivement changer sur un point de livraison.
+LIBELLE_REGION
+La région d'appartenance au point de livraison
+LIBELLE_TERRITOIRE
+Le territoire d'appartenance au point de livraison
+CODE_CONTRAT
+Le code du contrat de délégation de service public auquel est associé le point de livraison
+LIBELLE_CATEGORIE_ABONNE
+La catégorie de l'abonné (bâtiment collectif public / privé, industriel, professionnel, ...)
+DIAMETRE_NOMINAL
+Le diamètre nominal du compteur (plus le compteur est gros, plus la consommation doit être élevé)
+TYPE_ABAQUE
+Le type de méthode qui nous a permis de calculer la consommation (est-on sur du ML, sur du télérelevé, ...)
+MOIS_CONSO
+Le mois concerné par la consommation
+ANNEE_CONSO
+L'année de la consommation
+DATE_CONSO_MOIS
+La date liée à la consommation
+VOLUME_MOIS
+Le volume consommé sur le mois / année en question
+
+The table abonnements with the following columns:
+CLE_ABONNEMENT
+L'identifiant de l'abonnement
+DATE_ENTREE_LOCAL_ABONNEMENT
+La date d'entrée de l'abonné dans le logement
+DATE_SOUSCRIPTION_ABONNEMENT
+La date à laquelle l'abonné a souscrit un abonnement
+DATE_RESILIATION_ABONNEMENT
+La date de résiliation de l'abonné. Quand l'abonné est hors d'une période active (entre souscription et résiliation), il ne doit pas y avoir de consommations.
+"""
--- prompting/prompt.py
+++ prompting/prompt.py
@@ -12,7 +12,11 @@ from consts      import *
 from agent_utils import *
 
 
-def render_from_df__human__prompt_metadata_describer(
+def sanitize(s:str):
+	return s.replace('"', '`')
+
+
+def render_from_df__human_prompt_metadata_describer(
 	df                  : pd.DataFrame,
 	df_text_description : str,
 	sample_size         : int = 10
@@ -23,15 +27,18 @@ def render_from_df__human__prompt_metadata_describer(
 	df_info = "Result from df.describe():\n" + str(df.describe(include='all')).replace('\t',"|") + "\n\n"
 	# df_categorical_data_sample
 
-	df_sample = "Small samlple of the dataset:\n" + str(df.sample(sample_size)).replace('\t',"|") + "\n\n"
+	df_sample = "Small sample of the dataset:\n" + str(df.sample(sample_size)).replace('\t',"|") + "\n\n"
 
 	prompt_footer = """[INST]
 	Based on this information, please provide a JSON, with, for each column, a metadata summary. This JSON should be a list of dictionaries, and each dictionary should contain:
+		- source: the name of the dataset
 	    - name: feature name
 	    - data type: data type of the feature
 	    - description: a brief description of the feature
-	    - range: range of the feature (if applicable)
+	    - sample: an array of samples of the feature
 	    - nature: whether the data is quantitative cardinal, quantitative ordinal, categorical, or an ID key
+	    - range: range of the feature (if applicable)
+
 	Please provide only the uglified JSON and nothing else.[/INST]"""
 
 	prompt = (
@@ -43,73 +50,129 @@ def render_from_df__human__prompt_metadata_describer(
 	)
 	return prompt
 
-def generate_columns_dependencies_prompts(
-	databse_description : str
-) -> List[str]:
-	prompt = f"""I have a dataset with different tables. Here is the description of the tables and their columns: {databse_description}.
+def render_from_metadata__human_prompt_table_joiner(
+	tables_metadata : dict,
+) -> str:
+	prompt_header = """I have a dataset with different tables. Here is the description of the tables and their columns:\n"""
+	tables_info = ""
+	for table, metadata in tables_metadata.items():
+		tables_info += f"Table: {sanitize(table)}\n" + sanitize(metadata) + "\n\n"
+
+	prompt_footer = """[INST]
+	Please provide a JSON array of pairs-of-pairs of features that you think act as keys to join the tables. Each pair-of-pair should contain 4 strings, and look like `((feature1_in_combination,source_dataset_of_feature1), (feature2_in_combination,source_dataset_of_feature2))`. 
+	Please provide only the uglified JSON and nothing else.[/INST]"""
+	return prompt_header + tables_info + prompt_footer
+
+
+def render_columns_dependencies_prompts(
+	database_description : str
+) -> str:
+	prompt = f"""I have a dataset with different tables. Here is the description of the tables and their columns: {sanitize(database_description)}.
 		I want you to find the correlation between the tables and between the columns.
-		Your role is to return me a list of columns that should be correlated, or equal.
+		Your role is to return me a list of columns that should be correlated, uncorrelated, or anticorrelated.
 		I expect from you a json format that should look like:
 		[
-			{
-				"candidate_feature_combo": ["taille_du_batiment","releve1","releve2"],
-				"reason_why_it_is_a_candidate": "reason..."
-			},
+			{{
+				candidate_feature_combo: [[feature1_in_combination,source_dataset_of_feature1],[feature2_in_combination,source_dataset_of_feature2],etc for as many features as are needed for the combo],
+				reasoning: explanation as to why you think it is a candidate,
+				correlation: correlated/uncorrelated/anticorrelated
+			}},
 			...
 		]
+		Make sure to return only the uglified JSON.
 	"""
 	return prompt
 
 
-def generate_request_from_tables_dependencies(
-	dataset_description  : str,
-	columns_dependency : str
+def render_request_from_tables_dependencies(
+	dataset_description : str,
+	columns_dependency  : str
 ) -> str:
 	prompt = f"""
-		I have a dataset. Here is the description of the dataset: {dataset_description}.
+		I have a dataset. Here is the description of the dataset: {sanitize(dataset_description)}.
 		I have identified the following dependencies between columns.
 		Here is the dependency : {columns_dependency}.
 		I want to see all the lines that do not comply with the givent correlation.
 		Write me a SQL query that will highlight the potential problems in the problem.
+		Make sure to write only the SQL query.
 	"""
 	return prompt
 
-def parse_agent2_output(agent2_output : str) -> List:
-	return json.loads(agent2_output)
+def parse_agent3_output(agent3_output : str) -> List:
+	return json.loads(agent3_output)
 
 
 def main():
 	# bedrock         = boto3.client(service_name="bedrock",         region_name='us-west-2')
 	bedrock_runtime = boto3.client(service_name = "bedrock-runtime", region_name='us-west-2')
-	test_human_prompt = render_from_df__human__prompt_metadata_describer(
-		df                  = pd.read_csv(DEFAULT_CSV_FILE_PATH),
-		df_text_description = "This dataset contains contract information about water utility clients",
-	)
-	description_agent_prompt = combine_prompts(
-		system_prompt_metadata_describer,
-		test_human_prompt,
-	)
-	print(description_agent_prompt)
-	agent1_output = run_mistral_prompt(
-		bedrock_runtime = bedrock_runtime,
-		prompt          = description_agent_prompt,
-	)
-	print(agent1_output)
+	agent1_outputs  = []
+	for csv, desc in DEFAULT_CSVS.items():
+		metadata_prompt = render_from_df__human_prompt_metadata_describer(
+			df                  = pd.read_csv(csv),
+			df_text_description = desc,
+		)
+		description_agent_prompt = combine_prompts(
+			system_prompt_metadata_describer,
+			metadata_prompt,
+		)
+		print(description_agent_prompt)
+		agent1_output = run_mistral_prompt(
+			bedrock_runtime = bedrock_runtime,
+			prompt          = description_agent_prompt,
+		)
+		print(f"{agent1_output}")
+		agent1_outputs.append(agent1_output)
 
-
-	agent2_input = generate_columns_dependencies_prompts(agent1_output)
+	agent2_inputs = {
+		filename: agent1_output
+		for filename, agent1_output in zip(DEFAULT_CSVS, agent1_outputs)
+	}
+	tabling_prompt = render_from_metadata__human_prompt_table_joiner(
+		tables_metadata = agent2_inputs,
+	)
+	tablejoiner_agent_prompt = combine_prompts(
+		system_prompt_table_joiner,
+		tabling_prompt,
+	)
+	print(f"\n\n{tablejoiner_agent_prompt=}")
 	agent2_output = run_mistral_prompt(
 		bedrock_runtime = bedrock_runtime,
-		prompt          = agent2_input,
+		prompt          = tablejoiner_agent_prompt,
+	)
+	print(agent2_output)
+
+	agent1_output_folded = ",".join(agent1_outputs)
+	combo_columns_prompt = render_columns_dependencies_prompts(agent1_output_folded)
+	agent3_input = combine_prompts(
+		system_prompt_verif_feature_elicitation,
+		combo_columns_prompt,
+	)
+	print(f"\n\n{agent3_input=}")
+	agent3_output = run_mistral_prompt(
+		bedrock_runtime = bedrock_runtime,
+		prompt          = agent3_input,
 	)
 
+	print(f"\n\n{agent3_output=}")
+	agent3_output_fixed = agent3_output[agent3_output.find('['):]
+	print(f"\n\n{agent3_output_fixed=}")
+	# agent3_output_fixed = run_mistral_prompt(
+	# 	bedrock_runtime = bedrock_runtime,
+	# 	prompt          = "This JSON is invalid, please fix it: " + agent3_output_fixed,
+	# )
 	result = []
-	parsed_agent2_output = parse_agent2_output(agent2_output)
-	for dependency in parsed_agent2_output:
-		agen_sql_prompt = generate_request_from_tables_dependencies(agent1_output, dependency)
+	parsed_agent3_output = parse_agent3_output(agent3_output_fixed)
+	for dependency in parsed_agent3_output:
+		print(f"\n\n{dependency=}")
+		agent_sql_prompt = render_request_from_tables_dependencies(agent1_output, dependency)
+		agent4_input = combine_prompts(
+			system_prompt_sql_request,
+			agent_sql_prompt,
+		)
+		print(f"\n\n{agent4_input=}")
 		sql_query = run_mistral_prompt(
 			bedrock_runtime = bedrock_runtime,
-			prompt          = agen_sql_prompt,
+			prompt          = agent4_input,
 		)
 		result.append(sql_query)
 	
@@ -117,4 +180,7 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	results = main()
+	print(f"SQL results:\n")
+	for sql_query in results:
+		print(f"{sql_query}")
