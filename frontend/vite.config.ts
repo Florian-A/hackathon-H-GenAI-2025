97c0a4506f921213c77886c225d312610abbb478
Florian <git@florian-a.dev>
2025-02-01 18:51:10 +0100

    df

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -16,4 +15,7 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'build' // Change le dossier de build de 'dist' à 'build'
+  }
 });
